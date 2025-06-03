16dbd395b39bd92a688a1cf9710b4f04bf96a29e
Lucas Trindade <trindadebra@gmail.com>
2025-06-03 11:32:04 -0300

    footer updayed

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -2,13 +2,13 @@
 
 import Image from "next/image"
 import Link from "next/link"
-import { geradoresRoutes, validadoresRoutes, conversoresRoutes } from "./Sidebar"
+import { geradoresRoutes, validadoresRoutes, conversoresRoutes, arquivosRoutes } from "./Sidebar"
 
 export default function Footer() {
     return (
         <footer className="bg-white">
             <div className="mx-auto px-6 pt-16 pb-8 lg:px-8 mt-16">
-                <div className="xl:grid xl:grid-cols-4 xl:gap-8">
+                <div className="grid grid-cols-1 md:grid-cols-5 gap-8">
                     <div className="space-y-8">
                         <Link href="/">
                             <Image
@@ -20,45 +20,53 @@ export default function Footer() {
                             />
                         </Link>
                     </div>
-                    <div className="mt-16 grid grid-cols-1 gap-8 xl:col-span-3 xl:mt-0">
-                        <div className="md:grid md:grid-cols-3 md:gap-8">
-                            <div>
-                                <h3 className="text-sm font-semibold leading-6 text-gray-900">{geradoresRoutes.name}</h3>
-                                <ul role="list" className="mt-6 space-y-4">
-                                    {geradoresRoutes.children?.map((item) => (
-                                        <li key={item.name}>
-                                            <Link href={item.href} className="text-sm leading-6 text-gray-600 hover:text-main-500">
-                                                {item.name}
-                                            </Link>
-                                        </li>
-                                    ))}
-                                </ul>
-                            </div>
-                            <div className="mt-10 md:mt-0">
-                                <h3 className="text-sm font-semibold leading-6 text-gray-900">{validadoresRoutes.name}</h3>
-                                <ul role="list" className="mt-6 space-y-4">
-                                    {validadoresRoutes.children?.map((item) => (
-                                        <li key={item.name}>
-                                            <Link href={item.href} className="text-sm leading-6 text-gray-600 hover:text-main-500">
-                                                {item.name}
-                                            </Link>
-                                        </li>
-                                    ))}
-                                </ul>
-                            </div>
-                            <div className="mt-10 md:mt-0">
-                                <h3 className="text-sm font-semibold leading-6 text-gray-900">{conversoresRoutes.name}</h3>
-                                <ul role="list" className="mt-6 space-y-4">
-                                    {conversoresRoutes.children?.map((item) => (
-                                        <li key={item.name}>
-                                            <Link href={item.href} className="text-sm leading-6 text-gray-600 hover:text-main-500">
-                                                {item.name}
-                                            </Link>
-                                        </li>
-                                    ))}
-                                </ul>
-                            </div>
-                        </div>
+                    <div>
+                        <h3 className="text-sm font-semibold leading-6 text-gray-900">{geradoresRoutes.name}</h3>
+                        <ul role="list" className="mt-2 space-y-4">
+                            {geradoresRoutes.children?.map((item) => (
+                                <li key={item.name} className="mb-0">
+                                    <Link href={item.href} className="text-sm leading-6 text-gray-600 hover:text-main-500">
+                                        {item.name}
+                                    </Link>
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
+                    <div>
+                        <h3 className="text-sm font-semibold leading-6 text-gray-900">{validadoresRoutes.name}</h3>
+                        <ul role="list" className="mt-2 space-y-4">
+                            {validadoresRoutes.children?.map((item) => (
+                                <li key={item.name} className="mb-0">
+                                    <Link href={item.href} className="text-sm leading-6 text-gray-600 hover:text-main-500">
+                                        {item.name}
+                                    </Link>
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
+                    <div>
+                        <h3 className="text-sm font-semibold leading-6 text-gray-900">{conversoresRoutes.name}</h3>
+                        <ul role="list" className="mt-2 space-y-4">
+                            {conversoresRoutes.children?.map((item) => (
+                                <li key={item.name} className="mb-0">
+                                    <Link href={item.href} className="text-sm leading-6 text-gray-600 hover:text-main-500">
+                                        {item.name}
+                                    </Link>
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
+                    <div>
+                        <h3 className="text-sm font-semibold leading-6 text-gray-900">{arquivosRoutes.name}</h3>
+                        <ul role="list" className="mt-2 space-y-4">
+                            {arquivosRoutes.children?.map((item) => (
+                                <li key={item.name} className="mb-0">
+                                    <Link href={item.href} className="text-sm leading-6 text-gray-600 hover:text-main-500">
+                                        {item.name}
+                                    </Link>
+                                </li>
+                            ))}
+                        </ul>
                     </div>
                 </div>
                 <div className="mt-16 border-t border-gray-900/10 pt-8 sm:mt-20 lg:mt-24 flex flex-col md:flex-row gap-4 items-center justify-between">
